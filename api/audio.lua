b92e8324739516ec03c90de26813a1134344a349
IonutParau <ionut.alex.parau@gmail.com>
2021-11-14 21:29:12 +0200

    Test

--- api/audio.lua
+++
@@ -1,8 +0,0 @@
-audiocache = {}
-
-function playSound(sound)
-  if not audiocache[sound] then
-    audiocache[sound] = love.audio.newSource(sound, "static")
-  end
-  love.audio.play(audiocache[sound])
-end
